3b8212c842be200d87200c5853455b7923ae1680
Minh Le <duyminh.le310@gmail.com>
2023-01-31 02:19:30 -0500

    Clean up index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,14 @@
-import {StaticImageData} from 'next/image';
-
 import styles from '../styles/Home.module.scss'
-
-import HeroImage from '../components/HeroImage';
 
 import Section from '../layouts/Section';
 import NewArrivals from '../components/home_components/NewArrivals';
 
 
-import Image001JPG from '../assets/images/samples/001.jpg';
-import Image002JPG from '../assets/images/samples/002.jpg';
-import Image003JPG from '../assets/images/samples/003.jpg';
-import Image004JPG from '../assets/images/samples/004.jpg';
 import BestSellers from '../components/home_components/BestSellers';
 import { NextPageCustomized } from './_app';
 import { pageConfigs } from '../config';
 
-import { getHomePageData, ProductType, HomePageDataType } from '../database';
+import { getHomePageData, HomePageDataType } from '../database';
 
 
 /***************************
@@ -69,7 +61,6 @@ Home.pageConfig = pageConfigs.home;
 export const getServerSideProps = async () => {
   const response = await getHomePageData();
 
-  // TODO: create new API endpoint to fetch data from database: newArrivalProducts, bestSellerProducts
   return {
     props: {
       response
