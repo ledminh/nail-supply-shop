d4a92a89260cb69ee5c3902dc22eafc9dd39034a
Minh Le <duyminh.le310@gmail.com>
2023-01-31 21:55:51 -0500

    move admin to components

--- components/admin/index.tsx
+++ components/admin/index.tsx
@@ -1,70 +1,80 @@
-import Category from '../../components/admin_components/Category';
-import Product from '../../components/admin_components/Product';
-import Subtitles from '../../components/admin_components/Subtitles';
-import About from '../../components/admin_components/About';
-
-import styles from '../../styles/admin.module.scss';
-
-import {getAdminPageData, AdminPageDataType } from '../../database';
-
-import { NextPageCustomized } from '../_app';
-import { pageConfigs } from '../../config';
+export const Admin = () => {
+  return (
+    <div>
+      <h1>Admin</h1>
+    </div>
+  );
+};
 
 
-type AdminProps = AdminPageDataType; 
 
-const Admin: NextPageCustomized<AdminProps> = ({categories, products, aboutHtmlText} ) => {
+// import Category from '../admin_components/Category';
+// import Product from '../admin_components/Product';
+// import Subtitles from '../admin_components/Subtitles';
+// import About from '../admin_components/About';
+
+// import styles from '../../styles/admin.module.scss';
+
+// import {getAdminPageData, AdminPageDataType } from '../../database';
+
+// import { NextPageCustomized } from '../../pages/_app';
+// import { pageConfigs } from '../../config';
+
+
+// type AdminProps = AdminPageDataType; 
+
+// const Admin: NextPageCustomized<AdminProps> = ({categories, products, aboutHtmlText} ) => {
 
   
 
-  return (
-    <div className={styles.wrapper}>
-      <section className={styles.section}>
-        <Category
-          categories={categories}  
-          />
-      </section>
-      <section className={styles.section}>
-        <Product
-          categories={categories}
-          products={products}
-          />
-      </section>
-      <section className={styles.section + ' ' + styles.full}>
-        <Subtitles
-          subtitles={[]}
-          />
-      </section>
-      <section className={styles.section + ' ' + styles.full}>
-        <About
-          htmlText={aboutHtmlText}
-          />
-      </section>
-    </div>
-  ); 
-}
+//   return (
+//     <div className={styles.wrapper}>
+//       <section className={styles.section}>
+//         <Category
+//           categories={categories}  
+//           />
+//       </section>
+//       <section className={styles.section}>
+//         <Product
+//           categories={categories}
+//           products={products}
+//           />
+//       </section>
+//       <section className={styles.section + ' ' + styles.full}>
+//         <Subtitles
+//           subtitles={[]}
+//           />
+//       </section>
+//       <section className={styles.section + ' ' + styles.full}>
+//         <About
+//           htmlText={aboutHtmlText}
+//           />
+//       </section>
+//     </div>
+//   ); 
+// }
 
-export default Admin;
-
-
-/****************************
- * Customized page
- */
+// export default Admin;
 
 
-Admin.pageConfig = pageConfigs.admin;
+// /****************************
+//  * Customized page
+//  */
 
 
-/****************************
- * getServerSideProps
- */
-export const getServerSideProps = async () => {
-  const response = await getAdminPageData();
+// Admin.pageConfig = pageConfigs.admin;
 
 
-  return {
-    props: {
-      response 
-    }
-  }
-}
+// /****************************
+//  * getServerSideProps
+//  */
+// export const getServerSideProps = async () => {
+//   const response = await getAdminPageData();
+
+
+//   return {
+//     props: {
+//       response 
+//     }
+//   }
+// }
