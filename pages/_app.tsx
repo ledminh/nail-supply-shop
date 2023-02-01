0ac837f013b0aad1ff5faecd573e5b4048026466
Minh Le <duyminh.le310@gmail.com>
2023-01-31 22:10:08 -0500

    add try/catch to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,62 +24,72 @@ type AppPropsCustomized = AppProps & {
 
 export default function App({ Component, pageProps }: AppPropsCustomized) {
   
-  const pageConfig = Component.pageConfig; 
+  try {
 
-  const response:ResponseType<HomePageDataType|AboutPageDataType|ShopPageDataType|AdminPageDataType|CategoryPageDataType> = pageProps.response;
-
+    const pageConfig = Component.pageConfig; 
   
-  return (
-    <>
-      {/* 1. Check for error first */}
-      <ErrorLayout responses={[pageProps.response]}>
-          {
-            // 2. If it reaches here, no error found. Response[0] === 'success' is just for typescript doesn't complain
-            response[0] === 'success' &&
-            (
-              // 3. populate the layout with pageInfo and pageConfig
-              <MainLayout 
-                // pass the whole config object to MainLayout to traced back the parent page
-                pageConfig={pageConfig}
-                title={response[1].pageInfo.title}
-                description={response[1].pageInfo.description}
-                >
-                  {
-                    response[1].pageInfo.heroImage ?
-                      <HeroImage 
-                        subtitle={response[1].pageInfo.subtitle}
-                        title={response[1].pageInfo.title}
-                        
-                        image={
-                          response[1].pageInfo.heroImage.image
-                        }
-                        
-                        imgAltText={
-                          response[1].pageInfo.heroImage.alt
-                        }
-                        havePlaceholder={false}
-                        />: pageConfig?.heroImage?
+    const response:ResponseType<HomePageDataType|AboutPageDataType|ShopPageDataType|AdminPageDataType|CategoryPageDataType> = pageProps.response;
+  
+    
+    return (
+      <>
+        {/* 1. Check for error first */}
+        <ErrorLayout responses={[pageProps.response]}>
+            {
+              // 2. If it reaches here, no error found. Response[0] === 'success' is just for typescript doesn't complain
+              response[0] === 'success' &&
+              (
+                // 3. populate the layout with pageInfo and pageConfig
+                <MainLayout 
+                  // pass the whole config object to MainLayout to traced back the parent page
+                  pageConfig={pageConfig}
+                  title={response[1].pageInfo.title}
+                  description={response[1].pageInfo.description}
+                  >
+                    {
+                      response[1].pageInfo.heroImage ?
                         <HeroImage 
                           subtitle={response[1].pageInfo.subtitle}
                           title={response[1].pageInfo.title}
                           
                           image={
-                            pageConfig.heroImage.image
+                            response[1].pageInfo.heroImage.image
                           }
                           
                           imgAltText={
-                            pageConfig.heroImage.alt
+                            response[1].pageInfo.heroImage.alt
                           }
-                          havePlaceholder={true}
-                        />: null                       
-                  }
-                  {/* 4. pageInfo has no use here, but still being passed along with other page data */}
-                <Component {...pageProps.response[1]} />
-              </MainLayout>
-            )
-          }
-      </ErrorLayout>
-    </>
-  )
+                          havePlaceholder={false}
+                          />: pageConfig?.heroImage?
+                          <HeroImage 
+                            subtitle={response[1].pageInfo.subtitle}
+                            title={response[1].pageInfo.title}
+                            
+                            image={
+                              pageConfig.heroImage.image
+                            }
+                            
+                            imgAltText={
+                              pageConfig.heroImage.alt
+                            }
+                            havePlaceholder={true}
+                          />: null                       
+                    }
+                    {/* 4. pageInfo has no use here, but still being passed along with other page data */}
+                  <Component {...pageProps.response[1]} />
+                </MainLayout>
+              )
+            }
+        </ErrorLayout>
+      </>
+    )
+  }
+  catch(err) {
+    return (
+      <>
+        Error
+      </>
+    )
+  }
   
 }
